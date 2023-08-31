c7a04c7b94bb48e7874ed9a04a88f5a71b20a6d0
autum <ard5036@g.rit.edu>
2023-08-31 07:49:07 -0400

    HelloWorld Push

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!"); < test code
+            Console.WriteLine("Autumn Derhammer");
+            int a = 8;
+            Console.WriteLine(5 + 5 + a);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
