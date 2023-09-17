d0cbe0d190b944f584f4c580deccd2d3001550ed
autum <ard5036@g.rit.edu>
2023-09-17 08:37:47 -0400

    commit 9/17/2023

--- MadLibsP7/Program.cs
+++ MadLibsP7/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Threading;
+using System.Runtime.CompilerServices;
 
 namespace Madlibs
 {
@@ -12,11 +13,18 @@ namespace Madlibs
     {
         static void Main(string[] args)
         {
+            start:
             int numLibs = 0;
             int cntr = 0;
             int nChoice = 0;
+            string sChoice = null;
+            string userName = null;
 
             string finalStory = "";
+            string resultString = null;
+            string userResponse = null;
+
+
 
             StreamReader input;
 
@@ -52,8 +60,44 @@ namespace Madlibs
 
             input.Close();
 
-            // prompt the user for which Mad Lib they want to play (nChoice)
+            //ask the user for their name
+            Console.WriteLine("Welcome! Please enter your name!");
+            userName = Console.ReadLine();
 
+            ask:
+            //ask them if they want to play
+            Console.WriteLine("Do you want to play Mad Libs?");
+            userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "yes")
+            {
+
+            }
+
+            else if (userResponse.ToLower() == "no")
+            { //end program if they say no
+                Console.WriteLine("Goodbye then!");
+                System.Environment.Exit(1);
+            }
+            else
+            { //send them back to the question if they say anything else
+                Console.WriteLine("Please enter 'yes' or 'no'.");
+                goto ask;
+            }
+
+            // prompt the user for which Mad Lib they want to play (nChoice)
+            Console.WriteLine("Which story do you want to play?(0-5)");
+            sChoice = Console.ReadLine();
+
+            //try to convert nChoice to int
+            try
+            {
+                nChoice = Convert.ToInt32(sChoice);
+            }
+            catch //send them back to the beginning if they don't
+            {
+                Console.WriteLine("Please enter a number 0 - 5");
+                goto start;
+            }
             // split the Mad Lib into separate words
             string[] words = madLibs[nChoice].Split(' ');
 
@@ -62,7 +106,7 @@ namespace Madlibs
                 // if word is a placeholder
                 if (word[0] == '{')
                 {
-                    string replaceWord = word.Replace("{", "").Replace("}", "").Replace("_", " ");
+                    string replaceWord = word.Replace("{", " ").Replace("}", " ").Replace("_", " ");
                     // prompt the user for the replacement
                     Console.Write("Input a {0}: ", replaceWord);
                     // and append the user response to the result string
@@ -73,7 +117,13 @@ namespace Madlibs
                 {
                     finalStory += word;
                 }
+                
+
+                resultString = finalStory;
+
+                
             }
+            Console.WriteLine(resultString);
         }
     }
 }
