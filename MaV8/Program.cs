8121d87904c83ed93c6468e591441dc1538e60b9
autum <ard5036@g.rit.edu>
2023-09-18 10:51:27 -0400

    commit #8 9/18/23

---
+++ MaV8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaV8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string to hold user input
+            string userInput = null;
+            //prompt the user
+            Console.WriteLine("Please enter a string.");
+            userInput = Console.ReadLine();
+            //set it to lower
+            userInput = userInput.ToLower();
+            //replace
+            userInput = userInput.Replace("no", "yes");
+            Console.WriteLine(userInput);
+        }
+    }
+}
