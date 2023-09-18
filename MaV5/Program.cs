43d55a2cbfe64f3709b20b5c11ac38f76e00ce4d
autum <ard5036@g.rit.edu>
2023-09-18 10:17:03 -0400

    commit #5 9/18/23

---
+++ MaV5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaV5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z= 0;
+
+            double[,,] values = new double[x, y, z];
+            while (x >= -1 && x <= 1 &&  y >= -1 && y <= 4)
+            {
+                z = (3 * (y * y)) + (2 * x) - 1; 
+            }
+        }
+    }
+}
