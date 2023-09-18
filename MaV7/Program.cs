bf824f657676b917050baafa6d6a4b44449ec56f
autum <ard5036@g.rit.edu>
2023-09-18 10:42:43 -0400

    commit #7 9/18/23

---
+++ MaV7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaV7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create string for user input
+            string userInput = null;
+
+            //create char array to hold user input
+            char[] reverseArray = null;
+
+            //prompt the user
+            Console.WriteLine("Please input a string");
+            userInput = Console.ReadLine();
+
+            //convert to a CharArray
+            reverseArray = userInput.ToCharArray();
+
+            //reverse it
+            Array.Reverse(reverseArray);
+            Console.WriteLine(reverseArray);
+        }
+    }
+}
