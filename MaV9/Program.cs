cbb6c80d11f69850e9c756d2a299b702ca2494ec
autum <ard5036@g.rit.edu>
2023-09-19 08:46:09 -0400

    commit 9/19/23

---
+++ MaV9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaV9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create variable to hold the final result
+            string result = null;
+            //create variable for user input
+            string userInput = null;
+            //prompt user for string
+            Console.WriteLine("Please enter a string.");
+            userInput = Console.ReadLine();
+
+            //store user input into an array and split up the words
+            string[] inputArray = userInput.Split(' ');
+
+            //for each element in the array, put double quotes
+             for (int i = 0; i < inputArray.Length; i++) //could of also used a for each here
+            {
+                //stick double quotes on
+                inputArray[i] = "\"" + inputArray[i] + "\"";
+            }
+
+             //hold the final result in "result" variable by joining the array into a string
+             result = string.Join(" ", inputArray);
+
+            //print the result
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MathDelegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //delegate method is a data type
+            //data type we create ourselves 
+            //1.defind the data type based on the methods signeture
+               //delegate double MathFunction(double n1, double n2);
+            //2. declare the delegate method variable
+               //MathFunction processDivMult;
+            //3. point to the method we want to call
+               //processDivMult = new MathFunction(Multiply);
+            //4. call the method
+               //nAnswer = processDivMult(n1, n2)
+
+            //Func<double, double, double>processDivMult <-- generic data type, returns a value
+            //Action<> <--- does not return a value
+
+            //anyonomus code block
+            //processDivMult = delegate(double n1, double n2) //create a method with no name
+              //{
+              //double returnVal = 0
+              //Return Val = n1 * n2
+              //}
+
+            //lamda function => 
+            //processDivMult = (n1,n2) =>
+            //{ }
+        }
+    }
+}
