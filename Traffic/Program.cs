933da6e90b65bee1ceb04847e2c76a9472973ed2
autum <ard5036@g.rit.edu>
2023-10-09 09:00:43 -0400

    commit 10/9/2023

---
+++ Traffic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FreightTrain train = new FreightTrain();
+            //AddPassenger(train);
+        }
+        static void AddPassenger(IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+            Console.WriteLine(vehicleObject.ToString());
+
+        }
+    }
+}
