b4bb4dabdd1bcfff442f7c31579d8408f37ee0c3
autum <ard5036@g.rit.edu>
2023-10-12 07:52:45 -0400

    commit 10/12/23

---
+++ DefiningClassMems/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClassMems
+{
+    internal class Program
+    {
+       public class MyClass
+        {
+            private string myString;
+            public virtual string GetString()
+            {
+                return myString;
+            }
+
+            public string MyString
+            {
+                set
+                {
+                    myString = value;
+                }
+            }
+        }
+        public class MyDerivedClass : MyClass
+        {
+            public override string GetString()
+            {
+                return base.GetString() + " output from the derived class";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            new MyDerivedClass().GetString();
+        }
+    }
+}
