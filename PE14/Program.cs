968b2b908d475a3a52a53a5c69381f39a54d8fd1
autum <ard5036@g.rit.edu>
2023-10-18 13:45:07 -0400

    classy commit pe14

---
+++ PE14/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE14
+{
+    internal class Program
+    {
+
+        //# 1 on PE14 - classy
+        public abstract class BaseClass
+        {
+            //create a private feild
+            private string pField;
+
+            //create a red-write property
+            public string Field 
+            { 
+                get
+                { 
+                    return pField; 
+                } 
+                set 
+                { 
+                    pField = value;
+                } 
+            }
+        }
+
+        //#2 on classy
+
+        //create interface
+        public interface iClassy
+        {
+            void iMethod();
+        }
+
+        //create class1 to inherit from interface
+        public class oneClassy : iClassy
+        {
+            public void iMethod()
+            {
+                Console.WriteLine("This is from oneClassy");
+            }
+        }
+
+        //create 2nd class to inherit from interface
+        public class twoClassy : iClassy
+        {
+            public void iMethod() 
+            {
+                Console.WriteLine("This is from twoClassy");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            oneClassy classy = new oneClassy();
+            twoClassy classy2 = new twoClassy();
+
+            MyMethod(classy);
+            MyMethod(classy2);
+        }
+
+        //method that calls the interface
+        public static void MyMethod(iClassy classy)
+        {
+            classy.iMethod();
+        }
+    }
+}
