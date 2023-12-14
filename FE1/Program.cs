0d78328ff69f53edaf416b8b94c04fff7242a572
autum <ard5036@g.rit.edu>
2023-12-14 09:11:10 -0500

    stackclass

---
+++ FE1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FE1
+{
+    internal class Program
+    {
+        //stack class
+        public class MyStack
+        {
+            //create the stack item
+            public List<int> Stack = new List<int>();
+            public MyStack() 
+            {
+
+            }
+            //push
+            public void Push(int x) 
+            { 
+                Stack.Add(x);
+            }
+            //pop
+            public int Pop()
+            {
+                int lastVal = Stack.Count - 1;
+                int popped = Stack[lastVal];
+                Stack.RemoveAt(lastVal);
+                return popped;
+            }
+            //peek
+            public int Peek() 
+            {
+                return Stack[Stack.Count - 1];
+            }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
